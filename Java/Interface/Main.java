9ed1d66ff4f75a076541553e47de384713bf0111
hyunhosung <hhs04321@naver.com>
2024-08-18 20:27:08 +0900

    v1

---
+++ Java/Interface/Main.java
@@ -0,0 +1,24 @@
+package Java.Interface;
+
+interface BlackTV {
+    void print();
+}
+interface ColorTV extends BlackTV {
+    void colorprint();
+}
+class TV implements ColorTV {
+    @Override
+    public void print() {
+        System.out.println("흑백티비");
+    }
+    @Override
+    public void colorprint() {
+        System.out.println("컬러티비");
+    }
+}
+class Main {
+    public static void main(String[] args) {
+        ColorTV ctv = new TV();
+        ctv.colorprint();
+    }
+}
