17cb28a25eefd4ad9e024165815ff8c68860b427
hyunhosung <hhs04321@naver.com>
2024-08-18 19:24:09 +0900

    ㅍ2

--- C/Function/Main.c
+++ C/Function/Main.c
@@ -3,12 +3,32 @@
 void myFunction() {
     printf("Hello world\n");
 }
-void myFunction2() {
-    printf("second function\n");
+void myFunction2(int n) {
+    printf("second function\n 매개변수 : %d\n", n);
 }
-
+void myFunnc3(int n[5]) {
+    n[0] += 1;
+    n[1] += 1;
+    n[2] += 1;
+    n[3] += 1;
+    n[4] += 1;
+}
+int myFunc4(int n) {
+    return n + 1;
+}
 int main(void) {
     myFunction();
-    myFunction2();
+    myFunction2(10);
+    int n[5] = { 1, 2, 3, 4, 5 };
+    myFunnc3(n);
+
+    printf("%d\n", n[0]);
+    printf("%d\n", n[1]);
+    printf("%d\n", n[2]);
+    printf("%d\n", n[3]);
+    printf("%d\n", n[4]);
+
+    printf("%d", myFunc4(10));
+
     return 0;
 }
