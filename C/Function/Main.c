e73c26350c2a24dd4bebfa1eba4b7a5ea2fbb483
hyunhosung <hhs04321@naver.com>
2024-08-18 19:12:15 +0900

    v1

---
+++ C/Function/Main.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+void myFunction() {
+    printf("Hello world\n");
+}
+void myFunction2() {
+    printf("second function\n");
+}
+
+int main(void) {
+    myFunction();
+    myFunction2();
+    return 0;
+}
