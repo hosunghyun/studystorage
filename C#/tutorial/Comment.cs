7666d7a4a216ee33206145088a331a2e0659a983
hyunhosung <hhs04321@naver.com>
2024-09-07 09:50:24 +0900

    v2

---
+++ C#/tutorial/Comment.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main( String[] args ) {
+            /*
+             * 이것은
+             * 여러줄
+             * 주석입니다.
+             */
+            string name = "Kim";    // 이름
+            Console.WriteLine( name );
+            int age = 20;           // 나이
+            Console.WriteLine( age );
+        }
+    }
+}
