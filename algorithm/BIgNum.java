065182a67ac6e91b4e2b415e4f465007d637a582
hyunhosung <hhs04321@naver.com>
2024-12-04 15:13:45 +0900

    v4

---
+++ algorithm/BIgNum.java
@@ -0,0 +1,48 @@
+package algorithm;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class BIgNum {
+    public static void main(String[] args) {
+        String NMK = new String("5 8 3");
+        String N = new String("2 4 5 5 6");
+
+        String[] arr = NMK.split(" ");
+
+        int n = Integer.parseInt(arr[0]);
+        int m = Integer.parseInt(arr[1]);
+        int k = Integer.parseInt(arr[2]);
+
+        String[] arr2 = N.split(" ");
+        List<Integer> data = new ArrayList<>();
+        
+        for(String s : arr2) {
+            data.add(Integer.parseInt(s));
+        }
+
+        Collections.sort(data);
+
+        int result = 0;
+        int first = data.get(n - 1);    // 가장 큰 수
+        int second = data.get(n - 2);   // 두 번째로 큰 수
+
+        while (true) {
+            for(int i = 0; i < k; i++) {    // 가장 큰 수를 k 번 더하기
+                if(m == 0) {
+                    break;  // m이 0이라면 반복문 탈출
+                }
+                result += first;
+                m--;
+            }
+            if(m == 0) {
+                break;
+            }
+            result += second;
+            m--;
+        }
+
+        System.out.println(result);
+    }
+}
---
+++ algorithm/BigNumMulti.java
@@ -0,0 +1,19 @@
+package algorithm;
+
+class Multi {
+    public int prod(int u, int v) {
+        Long x, y, w, z;
+        int n, m;
+
+        n = String.valueOf(u).length() + String.valueOf(v).length();
+        if(u == 0 || v == 0) {
+            return 0;
+        }
+    }
+}
+
+public class BigNumMulti {
+    public static void main(String[] args) {
+        
+    }
+}
