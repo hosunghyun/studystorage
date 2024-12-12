f333e65fa8f35cb620d8e187650eeb5f4304bcfe
hyunhosung <hhs04321@naver.com>
2024-12-12 13:02:27 +0900

    v12

---
+++ algorithm/Graph.java
@@ -0,0 +1,12 @@
+package algorithm;
+
+public class Graph {
+    public static void main(String[] args) {
+        int INF = 987654321;
+        int[][] grapth = new int[][] {
+            {0, 7, 5},
+            {7, 0, INF},
+            {5, INF, 0}
+        };
+    }
+}
