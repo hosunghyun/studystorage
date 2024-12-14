9871c3b6330d7a50d368a8f6c6c7bdb22bc59407
hyunhosung <hhs04321@naver.com>
2024-12-14 12:31:19 +0900

    v14

--- algorithm/main.py
+++ algorithm/main.py
@@ -24,3 +24,37 @@ def BFS(x, y):
     return graph[n - 1][m - 1]
 
 print(BFS(0, 0))
+
+# 삽입 정렬 소스코드
+array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
+for i in range(1, len(array)):
+    for j in range(i, 0, -1):
+        if array[j] < array[j - 1]:
+            array[j], array[j - 1] = array[j - 1], array[j]
+        else:
+            break
+print(array)
+
+# 퀵 정렬
+array = [5, 7, 9, 0, 3, 1, 6, 2, 4, 8]
+def quick_sort(array, start, end):
+    if start >= end:
+        return
+    pivot = start
+    left = start + 1
+    right = end
+    while left <= right:
+        while left <= end and array[left] <= array[pivot]:
+            left += 1
+        while right > start and array[right] >= array[pivot]:
+            right -= 1
+        if left > right:
+            array[right], array[pivot] = array[pivot], array[right]
+        quick_sort(array, start, right - 1)
+        quick_sort(array, right + 1, end)
+        
+    quick_sort(array, 0, len(array) - 1)
+    print(array)
+    
+    # 파이썬 장점을 살린 퀵 정렬
+    
