ec6b41db629778c01e0994a2b507cb52f5929456
hyunhosung <hhs04321@naver.com>
2024-12-15 12:15:33 +0900

    v15

---
+++ algorithm/FindX.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class FindX {
+    public static void main(String[] args) {
+        String str = "oxooxoxxox";
+        List<Integer> answer = new ArrayList<>();
+        for(String s : str.split("x")) {
+            if(!s.contains("x"))
+                answer.add(s.length());
+        }
+        
+    }
+}
---
+++ algorithm/StringFind.java
@@ -0,0 +1,9 @@
+
+public class StringFind {
+    public static void main(String[] args) {
+        String myString = "ABBAA";
+        String pat = "AABB"; 
+        pat.replaceAll("A", "C").replaceAll("B", "A").replaceAll("C","B");
+        System.out.println((myString.contains(pat)) ? 1 : 0);
+    }
+}
