d939065a4c433faeb0746e407b6b83e8622df8e8
hyunhosung <hhs04321@naver.com>
2024-12-17 11:11:28 +0900

    v17

---
+++ algorithm/UpDown.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class UpDown {
+    public static void main(String[] args) {
+        int[] n = {15, 27, 12};
+        for(int i : n) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        Arrays.sort(n);
+        for(int i : n) {
+            System.out.print(i + " ");
+        }
+    }
+}
