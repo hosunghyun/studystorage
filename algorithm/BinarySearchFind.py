fd8e257ea3336a5565325a606819e35fbd3e7f2b
hyunhosung <hhs04321@naver.com>
2025-01-06 12:17:32 +0900

    v36

--- algorithm/BinarySearchFind.py
+++ algorithm/BinarySearchFind.py
@@ -15,6 +15,7 @@ def binary_search(array, target, start, end):
     
 # N(가게의 부품 개수) 입력
 n = int(input())
+
 # 가게에 있는 전체 부품 번호를 공백으로 구분하여 입력
 array = list(map(int, input().split()))
 array.sort()    # 이진 탐색을 수행하기 위해 사전에 정렬 수행
