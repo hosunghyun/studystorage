3b3dd8440eb8b3141c0435f82225bc6624939fe1
hyunhosung <hhs04321@naver.com>
2025-01-23 10:22:26 +0900

    v53

--- algorithm/Dijkstra1.py
+++ algorithm/Dijkstra1.py
@@ -23,6 +23,7 @@ for _ in range(m):
 def get_smallest_node():
     min_value = INF
     index = 0 # 가장 최단 거리가 짧은 노드(인덱스)
+    
     for i in range(1, n + 1):
         if distance[i] < min_value and not visited[i]:
             min_value = distance[i]
