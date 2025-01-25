1c900cdcb992adf77ddc2b0cb7c07e9868684756
hyunhosung <hhs04321@naver.com>
2025-01-25 11:21:59 +0900

    v55

--- algorithm/Dijkstra1.py
+++ algorithm/Dijkstra1.py
@@ -7,6 +7,7 @@ n, m = map(int, input().split())
 
 # 시작 노드 번호를 입력 받기
 start = int(input())
+
 # 각 노드에 연결되어 있는 노드에 대한 정보를 담는 리스트를 만들기
 graph = [[] for i in range(n + 1)]
 # 방문한 적이 있는지 체크하는 목적의 리스트를 만들기
