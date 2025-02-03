211936785eb6ed092fdea4563ea7b978a79cc962
hyunhosung <hhs04321@naver.com>
2025-02-03 20:25:51 +0900

    v63

--- algorithm/FindX.java
+++ algorithm/FindX.java
@@ -5,6 +5,7 @@ public class FindX {
     public static void main(String[] args) {
         String str = "oxooxoxxox";
         List<Integer> answer = new ArrayList<>();
+        
         for(String s : str.split("x")) {
             if(!s.contains("x"))
                 answer.add(s.length());
