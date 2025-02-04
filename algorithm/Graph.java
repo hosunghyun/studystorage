35bcf0a0882a4311f604c956de7479f4c86413e1
hyunhosung <hhs04321@naver.com>
2025-02-04 20:29:04 +0900

    v64

--- algorithm/Graph.java
+++ algorithm/Graph.java
@@ -3,6 +3,7 @@ package algorithm;
 public class Graph {
     public static void main(String[] args) {
         int INF = 987654321;
+        
         int[][] grapth = new int[][] {
             {0, 7, 5},
             {7, 0, INF},
