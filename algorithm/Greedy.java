f230e63a6df34429b7885b22df95731816982363
hyunhosung <hhs04321@naver.com>
2025-02-05 20:09:26 +0900

    v65

--- algorithm/Greedy.java
+++ algorithm/Greedy.java
@@ -3,6 +3,7 @@ public class Greedy {
         int[] coins = new int[] { 500, 100, 50, 10 };    // 동전의 종류
         int amount = 1260;  // 거스름돈
         int count = 0;      // 동전의 개수
+        
         for (int coin : coins) {
             count += amount / coin;
             amount %= coin;
