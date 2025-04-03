6bf45f91b1d5b25e160ea22932e592eb338b9eb3
hyunhosung <hhs04321@naver.com>
2025-04-03 21:38:58 +0900

    v114

--- algorithm/Strassen.java
+++ algorithm/Strassen.java
@@ -90,6 +90,7 @@ class StrassenMatrixMultiplication {
         int n = A.length;
         
         int[][] result = new int[n][n];
+        
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 result[i][j] = A[i][j] - B[i][j];
