be14bbf9b360ed676e29915cce0561a12c335370
hyunhosung <hhs04321@naver.com>
2025-04-13 11:45:27 +0900

    v122

--- algorithm/StringFind.java
+++ algorithm/StringFind.java
@@ -5,6 +5,7 @@ public class StringFind {
         String pat = "AABB"; 
         
         pat.replaceAll("A", "C").replaceAll("B", "A").replaceAll("C","B");
+        
         System.out.println((myString.contains(pat)) ? 1 : 0);
     }
 }
