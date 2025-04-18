2b045d81269a323b6c6d4202895221ef7c65848b
hyunhosung <hhs04321@naver.com>
2025-04-18 22:36:13 +0900

    v127

--- algorithm/Test1_2.java
+++ algorithm/Test1_2.java
@@ -10,6 +10,7 @@ class Test {
 public class Test1_2 {
     public static void main(String[] args) {
         Test test = new Test();
+        
         // n 개의 수로 구성된 리스트에서 m개의 가장 작은 수를 찾기
         int[] arr = new int[] {10, 7, 11, 5, 13, 8};
         int m = 3;
