e44517604e0870c7aeff424b8d33a020853d9cad
hyunhosung <hhs04321@naver.com>
2025-04-19 19:53:24 +0900

    v128

--- algorithm/Time.java
+++ algorithm/Time.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Time {
     public static void main(String[] args) {
         Scanner scr = new Scanner(System.in);
+        
         int N = scr.nextInt();
         int count = 0;
         for(int i = 0; i < (N + 1); i++ ) {
