b61d5bcd6209e8c2dc0b536f3d3d94070060128e
hyunhosung <hhs04321@naver.com>
2025-05-26 20:57:36 +0900

    v161

--- Java/CollectionFramework/Arrlst.java
+++ Java/CollectionFramework/Arrlst.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 public class Arrlst {
     public static void main(String[] args) {
         ArrayList<String> list = new ArrayList<>();
+
         list.add("Apple");
         list.add("Banana");
         list.add(1, "Orange"); // 인덱스 1에 요소 삽입
