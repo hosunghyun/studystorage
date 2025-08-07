3b4592b6dd4c0800b392e7167d10219fab8f5098
hyunhosung <hhs04321@naver.com>
2025-08-07 22:08:49 +0900

    v222

--- algorithm/BIgNum.java
+++ algorithm/BIgNum.java
@@ -30,6 +30,7 @@ public class BIgNum {
         int result = 0;
         
         int first = data.get(n - 1);    // 가장 큰 수
+        
         int second = data.get(n - 2);   // 두 번째로 큰 수
 
         while (true) {
